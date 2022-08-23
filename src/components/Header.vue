0ddcba8c723437fade1b449bc9bf7d0fd2d22569
Ken Hsu <s988662211@gmail.com>
2022-08-24 02:26:27 +0800

    Add Home / Header

---
+++ src/components/Header.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="navbar">
+  <a-menu v-model:selectedKeys="current" mode="horizontal">
+    <a-menu-item key="mail">
+      <template #icon>
+        <mail-outlined />
+      </template>
+      Avatar
+    </a-menu-item>
+
+    <a-sub-menu key="sub1">
+      <template #icon>
+        <setting-outlined />
+      </template>
+      <template #title>Catagories</template>
+      <a-menu-item-group title="Item 1">
+        <a-menu-item key="setting:1">Option 1</a-menu-item>
+        <a-menu-item key="setting:2">Option 2</a-menu-item>
+      </a-menu-item-group>
+      <a-menu-item-group title="Item 2">
+        <a-menu-item key="setting:3">Option 3</a-menu-item>
+        <a-menu-item key="setting:4">Option 4</a-menu-item>
+      </a-menu-item-group>
+    </a-sub-menu>
+
+    <a-menu-item key="app" disabled>
+      <template #icon>
+        <appstore-outlined />
+      </template>
+      About
+    </a-menu-item>
+
+    <a-menu-item key="alipay">
+      <a href="https://antdv.com" target="_blank" rel="noopener noreferrer">
+        Links
+      </a>
+    </a-menu-item>
+  </a-menu>
+  </div>
+</template>
+
+<script setup>
+import { MailOutlined, AppstoreOutlined, SettingOutlined } from '@ant-design/icons-vue';
+
+</script>
+
+<style lang="scss" scoped>
+  .navbar {
+    // height:80px;
+    // background-color: aqua;
+  }
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="layout">
+    <Header></Header>
+    <!-- <a href="https://vitejs.dev" target="_blank">
+      <img src="../assets/vite.svg" class="logo" alt="Vite logo" />
+    </a>
+    <a href="https://vuejs.org/" target="_blank">
+      <img src="../assets/vue.svg" class="logo vue" alt="Vue logo" />
+    </a> -->
+  </div>
+</template>
+
+<script setup>
+import Header from '../components/Header.vue';
+
+</script>
+
+<style lang="scss" scoped>
+  .layout {
+    // display: flex;
+    flex-direction: column;
+    min-height: 100vh;
+    background-color: #ccc;
+  }
+  .logo {
+    height: 6em;
+    padding: 1.5em;
+    will-change: filter;
+  }
+  .logo:hover {
+    filter: drop-shadow(0 0 2em #646cffaa);
+  }
+  .logo.vue:hover {
+    filter: drop-shadow(0 0 2em #42b883aa);
+  }
+</style>
