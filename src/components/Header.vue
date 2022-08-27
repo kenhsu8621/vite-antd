61c669ebc996e8cd6196c575e93db0bb8b84ef0c
kenhsu8621 <s988662211@gmail.com>
2022-08-28 03:21:21 +0800

    [feat] Update Header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -5,40 +5,6 @@
         <a href="" class="kh-logo"> <img src="../assets/images/kh-frame.svg" alt="kh-logo" width="50" /> </a>
       </a-col>
       <a-col :span="12" class="center">
-        <!-- <a-menu class="" v-model:selectedKeys="current" mode="horizontal">
-          <a-menu-item key="mail">
-            <template #icon>
-              <mail-outlined />
-            </template>
-            Avatar
-          </a-menu-item>
-
-          <a-sub-menu key="sub1">
-            <template #icon>
-              <setting-outlined />
-            </template>
-            <template #title>Catagories</template>
-            <a-menu-item-group title="Item 1">
-              <a-menu-item key="setting:1">Option 1</a-menu-item>
-              <a-menu-item key="setting:2">Option 2</a-menu-item>
-            </a-menu-item-group>
-            <a-menu-item-group title="Item 2">
-              <a-menu-item key="setting:3">Option 3</a-menu-item>
-              <a-menu-item key="setting:4">Option 4</a-menu-item>
-            </a-menu-item-group>
-          </a-sub-menu>
-
-          <a-menu-item key="app" disabled>
-            <template #icon>
-              <appstore-outlined />
-            </template>
-            About
-          </a-menu-item>
-
-          <a-menu-item key="alipay">
-            <a href="https://antdv.com" target="_blank" rel="noopener noreferrer"> Links </a>
-          </a-menu-item>
-        </a-menu> -->
         <a-space>
           <a class="nav-link">Home</a>
           <a class="nav-link">Catagories</a>
@@ -47,22 +13,19 @@
         </a-space>
       </a-col>
       <a-col :span="6" class="right">
-        <!-- <a-button shape="circle">
-          <template #icon><GlobalOutlined /></template>
-        </a-button> -->
         <div class="lang-select" :class="{ expand: expandLangList }">
           <GlobalOutlined
             :style="{ fontSize: '20px', position: 'absolute', top: '8px', right: '8px' }"
             @click="toggleLangList"
           />
           <div class="current-lang" v-if="!showLangList">
-            <span class="tw" v-if="language == 0">繁中</span>
+            <span class="tw" v-if="language == 0">中文</span>
             <span class="en" v-if="language == 1">EN</span>
           </div>
 
           <div class="lang-list" v-if="showLangList">
             <button class="tw" :class="language == 0 ? 'btn-active' : 'btn-inactive'" @click="setActiveLang(0)">
-              繁中
+              中文
             </button>
             <button class="en" :class="language == 1 ? 'btn-active' : 'btn-inactive'" @click="setActiveLang(1)">
               English
@@ -103,34 +66,23 @@
 </script>
 
 <style lang="scss" scoped>
-  $bg-half-opacity: rgba(255, 255, 255, 0.5);
-  .navbar {
-    height: 70px;
-    // border-bottom: 1px solid #ccc;
-    background-color: $bg-half-opacity;
-    filter: blur(0px);
-    box-shadow: 0 0 0.5em rgba(0, 0, 0, 0.25);
-    // background: linear-gradient(-45deg, #ee7752, #e73c7e, #23a6d5, #23d5ab);
-    // background-size: 400% 400%;
-    // animation: gradient 15s ease infinite;
+  @import "../assets/scss/variables.scss";
 
-    // @keyframes gradient {
-    //   0% {
-    //     background-position: 0% 50%;
-    //   }
-    //   50% {
-    //     background-position: 100% 50%;
-    //   }
-    //   100% {
-    //     background-position: 0% 50%;
-    //   }
-    // }
+  .navbar {
+    position: fixed;
+    z-index: 9;
+    width: 100%;
+    height: 70px;
+    background-color: $bgHalfOpacity;
+    filter: blur(0px);
+    box-shadow: $mainBoxShadow;
     .ant-col {
       line-height: 70px;
     }
 
     .center {
-      text-align: center;
+      display: flex;
+      justify-content: center;
       a {
         font-size: 20px;
         color: #213547;
@@ -145,16 +97,16 @@
 
     .right {
       .lang-select {
-        width: 80px;
+        width: 70px;
         line-height: 22px;
         position: absolute;
         padding: 6px;
         top: 16px;
         right: 15px;
-        color: #2173c9;
-        background-color: $bg-half-opacity;
+        color: $darkgrey;
+        background-color: $bgHalfOpacity;
         border-radius: 19px;
-        border: 1px solid #2173c9;
+        border: 1px solid $darkgrey;
         transition: all 0.2s ease-in-out;
 
         svg {
@@ -167,7 +119,7 @@
           font-size: 14px;
           font-weight: bold;
           line-height: 24px;
-          padding-left: 6px;
+          padding-left: 3px;
         }
 
         .en {
@@ -189,14 +141,14 @@
 
           .btn-active {
             color: #fff;
-            background-color: #2173c9;
-            border: 1px solid #2173c9;
+            background-color: $darkgrey;
+            border: 1px solid $darkgrey;
           }
 
           .btn-inactive {
-            color: #2173c9;
+            color: $darkgrey;
             background-color: transparent;
-            border: 1px solid #2173c9;
+            border: 1px solid $darkgrey;
           }
         }
       }
