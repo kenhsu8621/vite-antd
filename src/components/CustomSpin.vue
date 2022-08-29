33ddf55401055ac000894932c05a28ce0a57fe2c
Ken Hsu <s988662211@gmail.com>
2022-08-30 02:47:18 +0800

    [style] Change scrollDown icon

--- src/components/CustomSpin.vue
+++ src/components/CustomSpin.vue
@@ -1,84 +1,99 @@
 <script setup>
-  import { ref } from "vue";
-  defineProps({
-    size: Number,
-  });
+import { ref } from "vue";
+
+defineProps({
+  size: Number,
+});
 </script>
 
 <template>
   <div>
-    <span class="custom-spin dot-animation" :style="{ fontSize: size + 'px', width: size + 'px', height: size + 'px' }">
-      <i :style="{ width: size / 2 - 1 + 'px', height: size / 2 - 1 + 'px' }"></i>
-      <i :style="{ width: size / 2 - 1 + 'px', height: size / 2 - 1 + 'px' }"></i>
-      <i :style="{ width: size / 2 - 1 + 'px', height: size / 2 - 1 + 'px' }"></i>
-      <i :style="{ width: size / 2 - 1 + 'px', height: size / 2 - 1 + 'px' }"></i>
+    <span
+      class="custom-spin dot-animation"
+      :style="{
+        fontSize: size + 'px',
+        width: size + 'px',
+        height: size + 'px',
+      }"
+    >
+      <i
+        :style="{ width: size / 2 - 1 + 'px', height: size / 2 - 1 + 'px' }"
+      ></i>
+      <i
+        :style="{ width: size / 2 - 1 + 'px', height: size / 2 - 1 + 'px' }"
+      ></i>
+      <i
+        :style="{ width: size / 2 - 1 + 'px', height: size / 2 - 1 + 'px' }"
+      ></i>
+      <i
+        :style="{ width: size / 2 - 1 + 'px', height: size / 2 - 1 + 'px' }"
+      ></i>
     </span>
   </div>
 </template>
 
 <style lang="scss" scoped>
-  .custom-spin {
+.custom-spin {
+  position: absolute;
+  display: inline-block;
+  font-size: 20px;
+  width: 20px;
+  height: 20px;
+  top: 45vh;
+  left: 0;
+  right: 0;
+  margin: 0 auto;
+
+  i {
+    width: 9px;
+    height: 9px;
+    border-radius: 100%;
+    transform: scale(0.75);
+    display: block;
     position: absolute;
-    display: inline-block;
-    font-size: 20px;
-    width: 20px;
-    height: 20px;
-    top: 45vh;
-    left: 0;
-    right: 0;
-    margin: 0 auto;
+    opacity: 0.3;
+    animation: antSpinMove 1s infinite linear alternate;
+    transform-origin: 50% 50%;
 
-    i {
-      width: 9px;
-      height: 9px;
-      border-radius: 100%;
-      // background-color: #1890ff;
-      transform: scale(0.75);
-      display: block;
-      position: absolute;
-      opacity: 0.3;
-      animation: antSpinMove 1s infinite linear alternate;
-      transform-origin: 50% 50%;
-
-      &:nth-child(1) {
-        left: 0;
-        top: 0;
-        background-color: rgb(237, 175, 104);
-      }
-      &:nth-child(2) {
-        right: 0;
-        top: 0;
-        animation-delay: 0.4s;
-        background-color: rgb(39, 209, 135);
-      }
-      &:nth-child(3) {
-        right: 0;
-        bottom: 0;
-        animation-delay: 0.8s;
-        background-color: rgb(92, 92, 255);
-      }
-      &:nth-child(4) {
-        left: 0;
-        bottom: 0;
-        animation-delay: 1.2s;
-        background-color: rgb(115, 43, 170);
-      }
+    &:nth-child(1) {
+      left: 0;
+      top: 0;
+      background-color: rgb(237, 175, 104);
+    }
+    &:nth-child(2) {
+      right: 0;
+      top: 0;
+      animation-delay: 0.4s;
+      background-color: rgb(39, 209, 135);
+    }
+    &:nth-child(3) {
+      right: 0;
+      bottom: 0;
+      animation-delay: 0.8s;
+      background-color: rgb(92, 92, 255);
+    }
+    &:nth-child(4) {
+      left: 0;
+      bottom: 0;
+      animation-delay: 1.2s;
+      background-color: rgb(115, 43, 170);
     }
   }
+}
 
-  .dot-animation {
-    transform: rotate(45deg);
-    animation: antRotate 1.2s infinite linear;
-  }
+.dot-animation {
+  transform: rotate(45deg);
+  animation: antRotate 1.2s infinite linear;
+}
 
-  @keyframes antSpinMove {
-    to {
-      opacity: 1;
-    }
+@keyframes antSpinMove {
+  to {
+    opacity: 1;
   }
-  @keyframes antRotate {
-    to {
-      transform: rotate(405deg);
-    }
+}
+@keyframes antRotate {
+  to {
+    transform: rotate(405deg);
   }
+}
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,10 +2,24 @@
   <div class="layout">
     <Header></Header>
     <ModelViewer></ModelViewer>
-    <a-button @click="scrollDown">scroll down</a-button>
+    <DownCircleOutlined
+      class="scroll-down"
+      :style="{ fontSize: '30px' }"
+      @click="scrollDown"
+    />
 
-    <div style="font-size: 100px; color: aquamarine; text-align: center">Bottom Section</div>
-    <section style="font-size: 18px; color: aquamarine; text-align: center; width: 800px; margin: auto">
+    <div style="font-size: 100px; color: aquamarine; text-align: center">
+      Bottom Section
+    </div>
+    <section
+      style="
+        font-size: 18px;
+        color: aquamarine;
+        text-align: center;
+        width: 800px;
+        margin: auto;
+      "
+    >
       青山依舊在，幾度夕陽紅。慣看秋月春風。一壺濁酒喜相逢，浪花淘盡英雄。是非成敗轉頭空，滾滾長江東逝水，白髮漁樵江渚上，古今多少事，都付笑談中。
       滾滾長江東逝水，浪花淘盡英雄。是非成敗轉頭空，青山依舊在，幾度夕陽紅。白髮漁樵江渚上，慣看秋月春風。一壺濁酒喜相逢，古今多少事，都付笑談中。
       青山依舊在，幾度夕陽紅。慣看秋月春風。一壺濁酒喜相逢，浪花淘盡英雄。是非成敗轉頭空，滾滾長江東逝水，白髮漁樵江渚上，古今多少事，都付笑談中。
@@ -29,33 +43,51 @@
 </template>
 
 <script setup>
-  import { ref, onMounted } from "vue";
-  import Header from "../components/Header.vue";
-  import ModelViewer from "../components/ModelViewer.vue";
+import { ref, onMounted } from "vue";
+import Header from "../components/Header.vue";
+import ModelViewer from "../components/ModelViewer.vue";
+import { DownCircleOutlined } from "@ant-design/icons-vue";
 
-  onMounted(() => {
-    window.addEventListener("keydown", (event) => {
-      if (event.key == "Space") {
-        scrollDown();
-      }
-    });
+onMounted(() => {
+  window.addEventListener("keydown", (event) => {
+    if (event.key == "Space") {
+      scrollDown();
+    }
   });
+});
 
-  function scrollDown() {
-    console.log("scroll down");
-    window.scrollTo({ top: 700, behavior: "smooth" });
-  }
+function scrollDown() {
+  window.scrollTo({ top: 850, behavior: "smooth" });
+}
 </script>
 
 <style lang="scss" scoped>
-  .layout {
-    min-height: 100vh;
-    background: rgb(36, 36, 36);
+@import "../assets/scss/variables.scss";
 
-    .ant-btn {
-      position: absolute;
-      bottom: 0;
-      left: 47vw;
+.layout {
+  min-height: 100vh;
+  background: rgb(36, 36, 36);
+
+  .scroll-down {
+    position: absolute;
+    bottom: 3vh;
+    left: 0;
+    right: 0;
+    margin: 0 auto;
+    width: 50px;
+    text-align: center;
+    color: $mainLightgreen;
+    animation: pulse 1s infinite ease-in-out alternate;
+  }
+
+  @keyframes pulse {
+    from {
+      // bottom: 3vh;
+    }
+    to {
+      bottom: 2.5vh;
+      color: #fff;
     }
   }
+}
 </style>
