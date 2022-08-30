2079f2fec6178b84fbca99c699541c56c509c03c
kenhsu8621 <s988662211@gmail.com>
2022-08-30 15:58:15 +0800

    [style] Add section divider

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,24 +2,10 @@
   <div class="layout">
     <Header></Header>
     <ModelViewer></ModelViewer>
-    <DownCircleOutlined
-      class="scroll-down"
-      :style="{ fontSize: '30px' }"
-      @click="scrollDown"
-    />
+    <div class="section-divider"></div>
 
-    <div style="font-size: 100px; color: aquamarine; text-align: center">
-      Bottom Section
-    </div>
-    <section
-      style="
-        font-size: 18px;
-        color: aquamarine;
-        text-align: center;
-        width: 800px;
-        margin: auto;
-      "
-    >
+    <section class="bottom-section">
+      <div class="bottom-section-title">Bottom Section</div>
       青山依舊在，幾度夕陽紅。慣看秋月春風。一壺濁酒喜相逢，浪花淘盡英雄。是非成敗轉頭空，滾滾長江東逝水，白髮漁樵江渚上，古今多少事，都付笑談中。
       滾滾長江東逝水，浪花淘盡英雄。是非成敗轉頭空，青山依舊在，幾度夕陽紅。白髮漁樵江渚上，慣看秋月春風。一壺濁酒喜相逢，古今多少事，都付笑談中。
       青山依舊在，幾度夕陽紅。慣看秋月春風。一壺濁酒喜相逢，浪花淘盡英雄。是非成敗轉頭空，滾滾長江東逝水，白髮漁樵江渚上，古今多少事，都付笑談中。
@@ -36,58 +22,65 @@
       滾滾長江東逝水，浪花淘盡英雄。是非成敗轉頭空，青山依舊在，幾度夕陽紅。白髮漁樵江渚上，慣看秋月春風。一壺濁酒喜相逢，古今多少事，都付笑談中。
       青山依舊在，幾度夕陽紅。慣看秋月春風。一壺濁酒喜相逢，浪花淘盡英雄。是非成敗轉頭空，滾滾長江東逝水，白髮漁樵江渚上，古今多少事，都付笑談中。
       滾滾長江東逝水，浪花淘盡英雄。是非成敗轉頭空，青山依舊在，幾度夕陽紅。白髮漁樵江渚上，慣看秋月春風。一壺濁酒喜相逢，古今多少事，都付笑談中。
-      青山依舊在，幾度夕陽紅。慣看秋月春風。一壺濁酒喜相逢，浪花淘盡英雄。是非成敗轉頭空，滾滾長江東逝水，白髮漁樵江渚上，古今多少事，都付笑談中
+      青山依舊在，幾度夕陽紅。慣看秋月春風。一壺濁酒喜相逢，浪花淘盡英雄。是非成敗轉頭空，滾滾長江東逝水，白髮漁樵江渚上，古今多少事，都付笑談中。
     </section>
-    <code> 123456 Done </code>
   </div>
 </template>
 
 <script setup>
-import { ref, onMounted } from "vue";
-import Header from "../components/Header.vue";
-import ModelViewer from "../components/ModelViewer.vue";
-import { DownCircleOutlined } from "@ant-design/icons-vue";
-
-onMounted(() => {
-  window.addEventListener("keydown", (event) => {
-    if (event.key == "Space") {
-      scrollDown();
-    }
-  });
-});
-
-function scrollDown() {
-  window.scrollTo({ top: 850, behavior: "smooth" });
-}
+  import { ref, onMounted } from "vue";
+  import Header from "../components/Header.vue";
+  import ModelViewer from "../components/ModelViewer.vue";
 </script>
 
 <style lang="scss" scoped>
-@import "../assets/scss/variables.scss";
+  @import "../assets/scss/variables.scss";
 
-.layout {
-  min-height: 100vh;
-  background: rgb(36, 36, 36);
+  .layout {
+    min-height: 100vh;
+    background: rgb(36, 36, 36);
 
-  .scroll-down {
-    position: absolute;
-    bottom: 3vh;
-    left: 0;
-    right: 0;
-    margin: 0 auto;
-    width: 50px;
-    text-align: center;
-    color: $mainLightgreen;
-    animation: pulse 1s infinite ease-in-out alternate;
-  }
+    .section-divider {
+      height: 20px;
+      background: linear-gradient(
+        -90deg,
+        rgb(24, 252, 176),
+        rgba(127, 255, 212, 1),
+        rgba(42, 55, 70, 1),
+        rgba(36, 36, 36, 1),
+        rgba(42, 55, 70, 1),
+        rgba(127, 255, 212, 1),
+        rgb(24, 252, 176)
+      );
+      background-size: 500% 500%;
+      animation: gradient 10s ease-in-out infinite alternate;
 
-  @keyframes pulse {
-    from {
-      // bottom: 3vh;
+      @keyframes gradient {
+        0% {
+          background-position: 0% 50%;
+        }
+        50% {
+          background-position: 100% 50%;
+        }
+        100% {
+          background-position: 0% 50%;
+        }
+      }
     }
-    to {
-      bottom: 2.5vh;
-      color: #fff;
+
+    .bottom-section {
+      padding-top: 3vh;
+      font-size: 18px;
+      color: $mainGreen;
+      text-align: center;
+      width: 800px;
+      margin: auto;
+
+      .bottom-section-title {
+        font-size: 100px;
+        color: $mainGreen;
+        text-align: center;
+      }
     }
   }
-}
 </style>
