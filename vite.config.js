5a82e543dc51e3a3cae5a235a412386cac8d8bef
kenhsu8621 <s988662211@gmail.com>
2022-08-31 23:12:48 +0800

    [test] Deploy to Github Pages

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import vue from "@vitejs/plugin-vue"
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/vite-antd/",
   plugins: [vue()],
   define: {
     __VUE_I18N_FULL_INSTALL__: true,
