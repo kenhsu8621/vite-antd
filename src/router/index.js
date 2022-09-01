8d6c1fc5a49f76ded1a7c0f164158850aba64fbe
kenhsu8621 <s988662211@gmail.com>
2022-09-01 14:46:14 +0800

    [fix] Change router path to avoid build error

--- src/router/index.js
+++ src/router/index.js
@@ -5,23 +5,23 @@ const routes = [
   {
     path: "/vite-antd/",
     name: "Home",
-    component: () => import("/src/views/Home.vue")
+    component: () => import("../views/Home.vue")
   },
   {
     path: "/vite-antd/model_viewer",
     name: "ModelViewer",
-    component: () => import("/src/components/ModelViewer.vue")
+    component: () => import("../components/ModelViewer.vue")
   },
   // finally the default route, when none of the above matches: 
   {
     path: "/vite-antd/404",
     name: "PageNotFound",
-    component: () => import("/src/views/PageNotFound.vue")
+    component: () => import("../views/PageNotFound.vue")
   },
   {
     path: "/:pathMatch(.*)",
     name: "PageNotFound",
-    component: () => import("/src/views/PageNotFound.vue")
+    component: () => import("../views/PageNotFound.vue")
   }
 ]
 
